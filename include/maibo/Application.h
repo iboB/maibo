8e15bf718f27db68543c6eb1f327fb1951b443bd
Borislav Stanimirov <b.stanimirov@abv.bg>
2015-06-23 09:36:22 +0300

    Timer and FPS data

--- include/maibo/Application.h
+++ include/maibo/Application.h
@@ -28,9 +28,6 @@ namespace maibo
         // Function to be called for each appliacation frame
         void mainLoop();
 
-        // total number of frames the aplication has run
-        size_t totalFrames() const { return m_totalFrames; }
-
     protected:
         void startRunning() { m_isRunning = true; }
         void stopRunning() { m_isRunning = false; }
@@ -44,7 +41,31 @@ namespace maibo
     private:
         bool m_isRunning = false;
 
-        size_t m_totalFrames = 0;
+        //////////////////////////////////////
+        // time and fps stuff
+    public:
+
+        // time in ms since the application has started
+        uint32_t getTicks() const;
+
+        // total number of frames the aplication has run
+        uint32_t totalFrames() const { return m_totalFrames; }
+    private:
+        uint32_t m_currentFrameTime = 0; // start of current frame (ms)
+        uint32_t m_timeSinceLastFrame = 0; // used as delta for updates
+
+        uint32_t m_desiredFrameTime = 0; // the application will try to work at this time per frame
+
+        void updateFPSData();
+
+        uint32_t m_totalFrames = 0; // number of frames since the app has started
+        uint32_t m_lastFrameEnd = 0; // end time of the last frame
+        uint32_t m_lastFPSStatusUpdateTime = 0; // last time the fps status was updated
+        uint32_t m_lastFPSStatusUpdateFrame = 0; // frame number of the last time the status was updated
+
+        // results
+        uint32_t m_meanFrameTime;
+        uint32_t m_fps;
     };
 
     // Users must give body to these functions, creating, returning, and destroying
--- src/Application.cpp
+++ src/Application.cpp
@@ -8,8 +8,14 @@
 #include <maibo/Application.h>
 
 #include <maibo/TaskManager.h>
+#include <maibo/lib/high_res_clock.h>
+
+#if !defined(__EMSCRIPTEN__)
+#   include <thread>
+#endif
 
 using namespace maibo;
+using namespace std;
 
 Application::Application()
 {
@@ -56,6 +62,10 @@ void Application::endFrame()
 
 void Application::mainLoop()
 {
+    uint32_t now = getTicks();
+    m_timeSinceLastFrame = now - m_currentFrameTime;
+    m_currentFrameTime = now;
+
     beginFrame();
     handleInput();
     update();
@@ -63,4 +73,43 @@ void Application::mainLoop()
     endFrame();
 
     ++m_totalFrames;
+    updateFPSData();
+
+#if !defined (__EMSCRIPTEN__)
+    uint32_t frameLength = getTicks() - m_currentFrameTime;
+    if (frameLength < m_desiredFrameTime)
+    {
+        this_thread::sleep_for(chrono::milliseconds(m_desiredFrameTime - frameLength));
+    }
+#endif
+}
+
+void Application::updateFPSData()
+{
+    const uint32_t now = getTicks();
+    const uint32_t fpsStatusUpdateTimeDelta = now - m_lastFPSStatusUpdateTime;
+
+    if (fpsStatusUpdateTimeDelta > 1000) // update once per second
+    {
+        const uint32_t framesDelta = m_totalFrames - m_lastFPSStatusUpdateFrame;
+        m_meanFrameTime = fpsStatusUpdateTimeDelta / framesDelta;
+        m_fps = (framesDelta * 1000) / fpsStatusUpdateTimeDelta;
+
+        m_lastFPSStatusUpdateTime = now;
+        m_lastFPSStatusUpdateFrame = m_totalFrames;
+
+        //ostringstream sout;
+        //sout << "mean frame time: " << m_meanFrameTime << " ms | fps: " << m_fps;
+        //m_mainWindow->setTitle(sout.str().c_str());
+    }
+}
+
+uint32_t Application::getTicks() const
+{
+    // actually the time returned is since the first time this function is called 
+    // but this is very early in the execution time, so it's fine
+
+    static auto start = high_res_clock::now();
+    auto time = high_res_clock::now() - start;
+    return uint32_t(chrono::duration_cast<chrono::milliseconds>(time).count()) + 1;
 }

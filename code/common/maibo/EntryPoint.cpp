89543268ab2301d719dd6ee28e9411cba0f4c52e
Borislav Stanimirov <b.stanimirov@abv.bg>
2017-02-18 18:23:37 +0200

    Simple Windows console initialization

--- code/common/maibo/EntryPoint.cpp
+++ code/common/maibo/EntryPoint.cpp
@@ -63,18 +63,7 @@ int main(int argc, char* argv[])
 {
 #if defined(_WIN32)
     AllocConsole();
-
-    HANDLE handle_out = GetStdHandle(STD_OUTPUT_HANDLE);
-    int hCrt = _open_osfhandle((intptr_t)handle_out, _O_TEXT);
-    FILE* hf_out = _fdopen(hCrt, "w");
-    setvbuf(hf_out, NULL, _IONBF, 1);
-    *stdout = *hf_out;
-
-    HANDLE handle_in = GetStdHandle(STD_INPUT_HANDLE);
-    hCrt = _open_osfhandle((intptr_t)handle_in, _O_TEXT);
-    FILE* hf_in = _fdopen(hCrt, "r");
-    setvbuf(hf_in, NULL, _IONBF, 128);
-    *stdin = *hf_in;
+    freopen("CONOUT$", "w", stdout);
 #endif
 
     Application_CreateInstance(argc, argv);
